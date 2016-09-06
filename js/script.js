fd107d97d346dedbbe2f2ab87be7267d39acf10a
Asmaodeus <cadoz.olivier@gmail.com>
2016-09-06 15:04:25 +0200

    add a few of functionnaries

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 var sign = true;
 
+
 $(document).ready(function(){
   $('.square').click(function(){
-
+    if ($(this).hasClass('empty')) {
       if (sign === true) {
-        $(this).html('<p>X</p>');
+        $(this).html('<p>X</p>').toggleClass('empty full');
         sign = false;
       }
       else if (sign === false) {
-        $(this).html('<p>O</p>');
+        $(this).html('<p>O</p>').toggleClass('empty full');
         sign = true;
       }
+    }
+
   });
 
   $('.clear').click(function(){
-    $('.square').html('');
+    $('.square').html('').addClass('empty').removeClass('full');
     sign = true;
   });
 });
